5e416f8e6ecd7aa89b9eb041993b0b688f6fdaf8
SANKET VILAS GADHE <98447853+SanketGadhe@users.noreply.github.com>
2023-05-27 22:09:05 +0530

    Add files via upload

---
+++ Day1_arithmetic.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1, num2;
+    cout << "Enter num1:";
+    cin >> num1;
+    cout << "Enter num2:";
+    cin >> num2;
+    cout << "\nSum:" << num1 + num2;
+    cout << "\nSubstraction:" << num1 - num2;
+    cout << "\nMultiplication:" << num1 * num2;
+    cout << "\nDivision:" << num1 / num2;
+    cout << "\nModulo:" << num1 % num2;
+}
