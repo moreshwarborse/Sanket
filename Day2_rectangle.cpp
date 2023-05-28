38e47253b6b33e6fcf6bba24668094214b180a06
SANKET VILAS GADHE <98447853+SanketGadhe@users.noreply.github.com>
2023-05-28 23:07:09 +0530

    Day2_rectangle.cpp
    Solution Day 2

---
+++ Day2_rectangle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+  
+  int l,b;
+  int area,perimeter;
+  area=l*b;
+  perimter=2*(l+b);
+  cout<<"Area of rectangle: "<<area;
+  cout<<"Perimeter of rectangle: "<<perimeter;
+  
+  }
