9597abe26df17c1fc51aaa2a0c5df77398255bdb
SANKET VILAS GADHE <98447853+SanketGadhe@users.noreply.github.com>
2023-05-30 23:08:03 +0530

    Update Day4_asci_size.cpp

--- Day4_asci_size.cpp
+++ Day4_asci_size.cpp
@@ -4,15 +4,15 @@ int main(){
   char ch;
   cout<<"Enter character:";
   cin>>ch;
-  cout<<"Ascii value of "<<ch<<"is "<<int(ch);
+  cout<<"\nAscii value of "<<ch<<"is "<<int(ch);
   int i;
   float f;
   double d;
   long double ld;
-  cout<<"Size of int"<<sizeof(i);
+  cout<<"\nSize of int"<<sizeof(i);
 
-  cout<<"Size of float"<<sizeof(f);
-  cout<<"Size of char"<<sizeof(ch);
-  cout<<"Size of double"<<sizeof(d);
-  cout<<"Size of long double"<<sizeof(ld);
+  cout<<"\nSize of float"<<sizeof(f);
+  cout<<"\nSize of char"<<sizeof(ch);
+  cout<<"\nSize of double"<<sizeof(d);
+  cout<<"\nSize of long double"<<sizeof(ld);
   }
